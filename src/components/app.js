506d413fa43432a11d488066225d08a6cbaee92a
christophe12 <mutachristophe12@gmail.com>
2018-05-19 15:34:54 +0200

    changed app.js text

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                React starter
+                React Hackathon
             </div>
         )
     }
