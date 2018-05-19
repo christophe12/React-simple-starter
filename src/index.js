18e7d93f314d04487fae7e40a71c22660ba4f368
christophe12 <mutachristophe12@gmail.com>
2018-05-19 18:14:08 +0200

    added

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return(
+    <div>
+      React app
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('.container-page')
+);
+
+
+
+
