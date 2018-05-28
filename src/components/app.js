3f0e38db95c02309eed44269d9d75df76e863804
Fileille <nabfileille@gmail.com>
2018-05-28 19:50:56 +0200

    I added an image

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './header'
+const App = () => {
+	return(
+		<div>
+			<Header/>
+			 React App
+		</div>
+
+		);
+};
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+	return(
+		<ul>
+			<li>Davy</li>
+			<li>Michou</li>
+			<li>Peter</li>
+			
+		</ul>
+
+		);
+};
+
+export default Header;
