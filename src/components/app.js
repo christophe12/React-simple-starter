fed25e4d5555be87b6a1abf66a930f8a54cd4acb
Fileille <nabfileille@gmail.com>
2018-05-30 21:31:42 +0200

    these are dummy changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
 import Homepage from './views/homepage';
-
+import Homepage from './views/about';
+import Homepage from './views/product';
 const App = () => {
-    return(
-      <div>
-        <Homepage />
-      </div>
+    
+  return(
+      <Switch>
+        <Route path="/" exact component={Homepage} />
+        <Route path="/about" component={About} />
+        <Route path="/product" component={Product} />
+      </Switch>
     );
-};
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom'
 import App from './components/app';
 import '../assets/styles/main.scss';
-<<<<<<< HEAD
-=======
-
->>>>>>> ff4fbea6c46816059a5eae7005342c4bda668641
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.querySelector('.container-page')
 );
 
-<<<<<<< HEAD
-=======
 
 
-
->>>>>>> ff4fbea6c46816059a5eae7005342c4bda668641
